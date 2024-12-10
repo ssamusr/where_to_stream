17c56fdd474f1fe130542b09207ebd354f51b462
Samuel <samusr@gmail.com>
2024-12-10 12:02:58 +0000

    Fixed background of shows without background images

--- src/pages/ShowPage.tsx
+++ src/pages/ShowPage.tsx
@@ -18,19 +18,25 @@ export const ShowPage: React.FC = () => {
   })
 
   if (!show || !show.streamingOptions?.es) {
-    return <div>Cargando...</div> //TODO: Añadir loading o fallback
+    return <div>Cargando...</div>
   }
 
   const subscriptionPlatforms = getSubscriptionPlatform(
     show.streamingOptions.es,
   )
 
+  const backgroundUrl =
+    show.imageSet.horizontalBackdrop?.w1440 ||
+    show.imageSet.horizontalPoster?.w1440 ||
+    ''
+
   return (
     <div className='relative h-screen w-screen overflow-hidden'>
       <div
         className='absolute inset-0 h-full w-full bg-cover bg-center'
         style={{
-          backgroundImage: `url(${show?.imageSet.horizontalBackdrop.w1440 || show?.imageSet.horizontalPoster.w1440})`,
+          backgroundImage: `url(${backgroundUrl})`,
+          backgroundSize: 'contain',
         }}
       >
         <div className='absolute inset-0 bg-black bg-opacity-70'></div>
@@ -39,24 +45,26 @@ export const ShowPage: React.FC = () => {
       <Header />
       <section className='relative z-10 flex h-full max-w-[1600px] flex-col items-start justify-center px-4 text-center text-white sm:px-6 lg:px-8 xl:px-12'>
         <div>
-          <h1 className='mb-10 w-full max-w-[650px] text-pretty text-start text-3xl font-bold md:mb-1 md:text-6xl'>
+          <h1 className='mb-5 w-full max-w-[650px] text-pretty text-start text-4xl font-bold md:mb-1 md:text-6xl'>
             {show?.title}
           </h1>
-          <div className='my-5 flex flex-wrap gap-1 md:gap-2 lg:gap-4'>
+          <div className='my-4 flex flex-wrap gap-1 md:my-5 md:gap-2 lg:gap-4'>
             {show?.genres.map((genre) => (
               <span
                 key={genre.id}
-                className='rounded-md bg-gray-700 px-2 py-1 text-sm'
+                className='rounded-md bg-gray-700 px-2 py-1 text-sm lg:text-base'
               >
                 {genre.name}
               </span>
             ))}
           </div>
-          <p className='w-full max-w-[650px] text-pretty text-start text-gray-100 md:block md:text-lg lg:text-xl'>
+          <p className='w-full max-w-96 text-pretty text-start text-base text-gray-100 md:block lg:max-w-[650px] lg:text-xl'>
             {show?.overview ? show.overview : show?.title}
           </p>
 
-          <h2 className='my-4 text-start text-lg'>You can watch on:</h2>
+          <h2 className='my-4 text-start text-base lg:text-lg'>
+            You can watch on:
+          </h2>
           <div className='mt-2 flex gap-2'>
             {subscriptionPlatforms.map((platform) => {
               const matchingIcon = getPlatformIcon(platform.service.name)
@@ -67,7 +75,7 @@ export const ShowPage: React.FC = () => {
                   href={platform.link}
                   target='_blank'
                   rel='noopener noreferrer'
-                  className='block'
+                  className='block md:hover:scale-110'
                 >
                   {matchingIcon ? (
                     <img
@@ -87,3 +95,5 @@ export const ShowPage: React.FC = () => {
     </div>
   )
 }
+
+//TODO: Añadir loading o fallback
