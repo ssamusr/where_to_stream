f8666fe94c3f74eddc61ead6e567ce406119abea
Samuel <samusr@gmail.com>
2024-12-11 18:26:18 +0000

    Removed all console log

--- src/services/getShowById.ts
+++ src/services/getShowById.ts
@@ -1,7 +1,6 @@
 import { api } from './config'
 
 export const getShowById = async (id: string) => {
-  console.log('API Key:', import.meta.env.VITE_API_KEY)
   const { data } = await api.get(`shows/${id}`, {
     params: {
       country: 'es',
--- src/services/searchShowByTitle.ts
+++ src/services/searchShowByTitle.ts
@@ -1,8 +1,6 @@
 import { api } from './config'
 
 export const searchShowByTitle = async (query?: string) => {
-  console.log('API Key:', import.meta.env.VITE_API_KEY)
-
   const { data } = await api.get('shows/search/title', {
     params: {
       country: 'es',
