dedd8b2c8688323520c6a0c2872e95d162968147
Samuel <samusr@gmail.com>
2024-12-11 16:35:24 +0000

    Added console log to fix a bug on production

--- src/services/searchShowByTitle.ts
+++ src/services/searchShowByTitle.ts
@@ -1,6 +1,8 @@
 import { api } from './config'
 
 export const searchShowByTitle = async (query?: string) => {
+  console.log('API Key:', import.meta.env.VITE_API_KEY)
+
   const { data } = await api.get('shows/search/title', {
     params: {
       country: 'es',
